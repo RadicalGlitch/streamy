80cba69c92700f4ef1c3ee67eb8940ebb5ba1af0
Rishabh <rishabh.z3im@gmail.com>
2020-09-12 00:17:32 +0530

    ready for deployment

---
+++ src/components/streams/StreamDelete.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Modal from "../Modal";
+import createBrowserHistory from "../../history";
+import { connect } from "react-redux";
+import { fetchStream, deleteStream } from "../../actions";
+import { Link } from "react-router-dom";
+
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params;
+
+    return (
+      //  <>[content]</> is equivalent to <React.Fragment>[content]</React.Fragment>
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`;
+  }
+
+  render() {
+    return (
+      <Modal
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => createBrowserHistory.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+import _ from "lodash";
+
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h3>Edit your Stream</h3>
+        <StreamForm
+          // _.pick lodash method only submits given key value pairs instead of all key value pair
+          // of the this.props.stream object. So that we don't pass stream id and user id during edit.
+          // we can also pass just an object with desired key value pairs to avoid lodash which will look like - {{object}} LOL
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
